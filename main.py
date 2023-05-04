e885a8a36b844013fd3fb59d26ad7ba5c2d9589a
NamesMark <markfirn@gmail.com>
2023-05-04 18:01:36 +0300

    added calc

--- main.py
+++ main.py
@@ -17,8 +17,18 @@ def decToVicz(decimal, vicz): # prop is int
     else:
         return vicz
 
+def processInput(input): # prop is string
+    block = ''
+    for i in range(len(input)):
+        if input[i] != '+' and input[i] != '-' and input[i] != '*' and input[i] != '/':
+            block += input[i]
+    
+
 
 testValue = viczToDec('ČAČ')
 print(testValue)
 
 print(decToVicz(5814, ''))
+
+print ('E * É * Ě = '),
+print(decToVicz(viczToDec('E')*viczToDec('É')*viczToDec('Ě'),''))
